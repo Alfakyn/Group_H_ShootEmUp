505e3f4ff84efda37c786cf939b1188f49890364
ChrisIdris <Christoffer.Elidrissi.6004@student.uu.se>
2020-02-17 01:52:19 +0100

    Tutorial sprites added
    Tutorial sprite added + animation
    changed waves timer to  recomended lowest to allow tutorial time to be seen before game starts

---
+++ Assets/Scripts/TutorialBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialBehaviour : MonoBehaviour
+{
+    Vector3 speed;
+    float camera_half_width;
+    float sprite_offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite_offset = 5.0f; // rough estimate
+        speed.Set(-0.003f, 0, 0);
+        camera_half_width = Camera.main.orthographicSize * Camera.main.aspect;    //Multiply the half-height by the aspect ration to get the half-width
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x + sprite_offset <= -camera_half_width)
+        {
+            Destroy(gameObject);
+        }
+        moveTutorial();
+    }
+    void moveTutorial()
+    {
+        transform.position += speed;
+    }
+}
