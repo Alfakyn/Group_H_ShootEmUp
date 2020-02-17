562b726e5b976d6459567ab8ff8d2fd25db051c6
MysanB99 <60471111+MysanB99@users.noreply.github.com>
2020-02-17 08:36:10 +0100

    More SFX
    torpedo explosion,

--- Assets/Scenes/Menu/Scripts/SoundManager.cs
+++ Assets/Scenes/Menu/Scripts/SoundManager.cs
@@ -6,6 +6,8 @@ public class SoundManager : MonoBehaviour
     public static AudioClip testMusic;
     public static AudioClip shootingTorpedo;
     public static AudioClip shootingBullet;
+    public static AudioClip hitTorpedo;
+    //public static AudioClip hitBullet;
 
     static AudioSource audioSource;
 
@@ -18,8 +20,10 @@ public class SoundManager : MonoBehaviour
     {
         testSound = Resources.Load<AudioClip>("SFX/Noice");  //Instantiate every audio file
         testMusic = Resources.Load<AudioClip>("Music/testMusic");
-        shootingTorpedo = Resources.Load<AudioClip>("SFX/SFX Weapons/Torpedo/Missile_sonic");
-        shootingBullet = Resources.Load<AudioClip>("SFX/SFX Weapons/Bullet/secondary_scuba");
+        shootingTorpedo = Resources.Load<AudioClip>("SFX/Weapons/Torpedo/Missile_sonic");
+        shootingBullet = Resources.Load<AudioClip>("SFX/Weapons/Bullet/secondary_scuba");
+        hitTorpedo = Resources.Load<AudioClip>("SFX/Weapons/Torpedo/missile_explosion");
+        //hitBullet = Resources.Load<AudioClip>("SFX/Weapons/Bullet/hit_bullet");
 
         audioSource = GetComponent<AudioSource>();
     }
