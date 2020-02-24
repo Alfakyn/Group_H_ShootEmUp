2efd9b2188f3dbfae84ba3b876b3e8957e08051d
Simon Zeidan <simon.z.mellkvist@hotmail.com>
2020-02-24 13:27:14 +0100

    Pause Menu Version 1.1 + first in-game music added

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.playMusic(SoundManager.gameMusic1);
+    }
+}
