b41509763c27f520d5cb17a0012644684e0909cb
ChrisIdris <Christoffer.Elidrissi.6004@student.uu.se>
2020-02-27 13:24:18 +0100

    Pufferfish script

--- Assets/Scripts/Pufferfish/PufferfishBehaviour.cs
+++ Assets/Scripts/Pufferfish/PufferfishBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pufferfish : MonoBehaviour
+public class PufferfishBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
